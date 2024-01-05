abcf396af53bfdd897d8ce9794c23932930b1f0e
PanduWahyudi <panduwahyudi12@gmail.com>
2024-01-05 14:35:32 +0700

    fixing image

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -1,61 +1,18 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import Image from "next/image";
+import mountains from "/public/assets/latar.png";
 
-interface FormData {
-  nama: string;
-  email: string;
-  password: string;
-  file: FileList;
-}
-
-const FormComponent: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit = (data: FormData) => {
-    console.log(data);
-    
-    // Lakukan sesuatu dengan data yang dikirim
-  };
-
+export default function Background() {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Nama:</label>
-        <input {...register("nama", { required: "Nama wajib diisi" })} />
-        {errors.nama && <span>{errors.nama.message}</span>}
-      </div>
-
-      <div>
-        <label>Email:</label>
-        <input {...register("email", { required: "Email wajib diisi" })} />
-        {errors.email && <span>{errors.email.message}</span>}
-      </div>
-
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          {...register("password", { required: "Password wajib diisi" })}
-        />
-        {errors.password && <span>{errors.password.message}</span>}
-      </div>
-
-      <div>
-        <label>Upload File:</label>
-        <input type="file" {...register("file")} />
-        {errors.file && <span>{errors.file.message}</span>}
-      </div>
-
-      <div>
-        <button type="submit">Kirim</button>
-      </div>
-    </form>
+    <Image
+      alt="Mountains"
+      src={mountains}
+      placeholder="blur"
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{
+        objectFit: "cover",
+      }}
+    />
   );
-};
-
-export default FormComponent;
+}
