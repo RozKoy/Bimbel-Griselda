42acef0c9f6710d3a1b1b8079f31d526c1608ffc
PanduWahyudi <panduwahyudi12@gmail.com>
2024-01-06 05:25:16 +0700

    fix Services Pages

--- src/components/landing-pages/CardServices.tsx
+++ src/components/landing-pages/CardServices.tsx
@@ -6,9 +6,9 @@ interface CardServicesProps {
 
 const CardServices: React.FC<CardServicesProps> = ({ title, description }) => {
   return (
-    <div className="bg-white rounded-tl-[28px] rounded-tr-[10px] flex flex-col space-y-6 items-center sm:py-8 sm:px-2 ">
+    <div className="bg-white rounded-t-[28px] rounded-b-[8px] hover:shadow-[0px_2.796px_27.956px_0px] hover:shadow-[#0065FF] cursor-pointer flex flex-col space-y-6 items-center sm:py-8 sm:px-2">
       <div className="flex justify-center items-center space-x-6">
-        <div className="bg-[#FFB700] p-3.5 flex justify-center items-center rounded-xl">
+        <div className="bg-[#FFB700] p-3.5 flex justify-center items-center rounded-xl ">
           <Education />
         </div>
         <p className="font-bold sm:text-4xl">{title}</p>
