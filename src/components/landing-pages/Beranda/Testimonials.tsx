b2a3a8923d9b8986f53902ba8b31d8fa556ac8fe
PanduWahyudi <panduwahyudi12@gmail.com>
2024-01-06 13:17:06 +0700

    Add Testimonials Pages

--- src/components/landing-pages/Beranda/Testimonials.tsx
+++ src/components/landing-pages/Beranda/Testimonials.tsx
@@ -1,7 +1,30 @@
+import CardTestimonials from "../CardTestimonials";
 const Testimonials = () => {
   return (
-    <div>
-      <h1>Testimonials</h1>
+    <div className="bg-[#FFB700] w-full h-screen sm:py-14 sm:px-14">
+      <div className=" w-auto h-full flex flex-col items-center sm:space-y-28 ">
+        <div className="flex flex-col space-y-2">
+          <p className="text-[#FFF] text-center font-extrabold sm:text-5xl">
+            Testimonials
+          </p>
+          <p className="text-center text-[#FFF] font-medium sm:text-lg sm:w-[535px]">
+            Most of them buy positive feedback for us which is none other than
+            the best service we provide
+          </p>
+        </div>
+        <div className="flex sm:space-x-6">
+          <CardTestimonials
+            nama="Pandu"
+            job="Programmers"
+            testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu consectetur elementum id massa."
+          />
+          <CardTestimonials
+            nama="Pandu"
+            job="Programmers"
+            testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu consectetur elementum id massa."
+          />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/landing-pages/CardTestimonials.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import user from "/public/assets/user.png";
+import Comma from "./Icons/CommaIcon";
+
+interface CardTestimonialsProps {
+  nama?: string;
+  job?: string;
+  testimony?: string;
+}
+
+const CardTestimonials: React.FC<CardTestimonialsProps> = ({
+  nama,
+  job,
+  testimony,
+}) => {
+  return (
+    <div className=" h-[300px] relative flex items-end">
+      <div className="absolute top-1 left-[40%]">
+        <Image
+          src={user}
+          width={70}
+          height={40}
+          alt="user"
+          className="rounded-full "
+        />
+      </div>
+      <div className="w-full h-5/6  bg-[#FFF] rounded-br-[20.803px] rounded-tl-[20.803px] flex flex-col items-center sm:py-10 sm:px-2 sm:space-y-4  ">
+        <div>
+          <p className="text-center font-medium sm:text-[26px]">{nama}</p>
+          <p className="text-center font-medium text-[#7C7C7C] sm:text-[17px]">
+            {job}
+          </p>
+        </div>
+        <div>
+          <Comma />
+          <p className="text-center text-[#2C3546] font-light sm:w-[343px] sm:text-base">
+            {testimony}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardTestimonials;
