648a3f698c550b6cddf69a9efadc3d4bb832b9be
randaru09 <ndaruanggoro123@gmail.com>
2024-02-01 16:01:59 +0700

    check RefreshToken di refresh()

--- src/utils/useRefreshToken.tsx
+++ src/utils/useRefreshToken.tsx
@@ -1,21 +1,34 @@
+import { useRouter } from "next/router";
 import { axiosInstance } from "../utils/axios";
 import useLocalStorage from "./useLocalStorage";
 
 export const useRefreshToken = () => {
-  const [refreshToken] = useLocalStorage("refreshToken", "");
+  // const [refreshToken] = useLocalStorage("refreshToken", "");
+  const [refreshToken, setRefreshToken] = useLocalStorage("refreshToken", "");
   const [accessToken, setAccessToken] = useLocalStorage("accessToken", "");
+  const router = useRouter();
+
+  const logout = () => {
+    setRefreshToken("");
+    setAccessToken("");
+    router.push("/login");
+  };
 
   const refresh = async () => {
-    const response = await axiosInstance.post(
-      "/auth/admin/refresh-access-token",
-      {
-        refresh_token: refreshToken,
+    try{
+      const response = await axiosInstance.post(
+        "/auth/admin/refresh-access-token",
+        {
+          refresh_token: refreshToken,
+      }
+      );
+      setAccessToken(response.data.data.access_token);
+      return response.data.data.access_token;
+    }catch(e:any){
+      if(e.response.status === 403){
+        logout();
+      }
     }
-    );
-
-    setAccessToken(response.data.data.access_token);
-
-    return response.data.data.access_token;
   };
 
   return refresh;
