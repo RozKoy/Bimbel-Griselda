313332bd74a7c4b4a0d31a50c396de273de1386c
Rozin <okkoy.1401@gmail.com>
2024-02-09 23:51:55 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-	// remotePatterns: [
-	// 	{
-	// 		hostname: 'localhost',
-	// 	}
-	// ]
-  // },
   async rewrites() {
     return [
       {
         source: "/api-backend/:path*",
-        // destination: `http://localhost:3001/api/:path*`,
-        destination: `http://be-griselda:3001/api/:path*`,
+        destination: `https://backend.bimbelgriselda.com/api/:path*`,
       },
     ];
   },
